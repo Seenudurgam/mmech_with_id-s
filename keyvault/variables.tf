7d7894a1ac338772b91cdb1abf50a08a917786f9
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-23 14:43:08 +0530

    removed subscription details

--- keyvault/variables.tf
+++ keyvault/variables.tf
@@ -15,25 +15,6 @@ variable "resource_group_location" {
   description = "The default Azure region for the resource provisioning"
   default = "West Europe"
 }
-
-variable "subscription_id" {
-  type        = string
-  description = "subscription ID."
-  default = "9657d952-ba4f-4588-8ada-bf25a4524929"
-}
-
-variable "client_id" {
-  type        = string
-  description = "client ID"
-  default = "a044fb05-dff5-4101-a571-3082d2bb761f"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "client secret"
-  default = "lWbltKXG_8e.9baX-sKnT1r4GvCK/atJ"
-}
-
 variable "tenant_id" {
   type        = string
   description = "tenant id"
